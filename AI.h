67ff8b2640b307edca949daef2a9cf4504859b8d
Mincu Dragos <mincu.dragos@gmail.com>
2015-11-01 07:37:27 +0200

    AI class

---
+++ AI.h
@@ -0,0 +1,20 @@
+#ifndef __AI_H_
+#define __AI_H_
+
+class AI {
+public:
+	AI(Game&, Danger&);
+	virtual ~AI();
+
+	std::pair<int, bool> getBestMove();
+private:
+	double getScore(int, int, bool);
+
+	double area(int, int);
+	double survival(int, int);
+
+	Game _game;
+	Danger _danger;
+};
+
+#endif
